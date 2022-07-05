c18b88caa69ff16865acf0a306a336af54303579
Frank Weijers <frank@weijers.io>
2022-07-05 23:29:12 +0200

    Include resource config

--- src/resource/dto/resource-parent-type.enum.ts
+++ src/resource/dto/resource-parent-type.enum.ts
@@ -1,9 +1,6 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 // Import ResourceParentType from config to allow each implementing application to define their own types
-// TODO: Find better way to allow for importing types from project
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import { ResourceParentType } from '@config/appvise/resource';
 
 export { ResourceParentType };
